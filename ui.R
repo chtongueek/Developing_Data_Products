a5ceca07fef18ec5e3a7319cdf32766b0c366130
chtongueek <chtongueek@users.noreply.github.com>
2015-06-19 18:37:06 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -13,7 +13,10 @@ shinyUI(fluidPage(
                   "Number of iterations:",
                   min = 2,
                   max = 1000,
-                  value = 500)
+                  value = 500),
+      "The app calculates the means of 40 random exponentials for however many iterations the user wants.
+       Simply use the sliderbar to adjust the number of iterations, and the app will update the distribution
+       as well as the theoretical normal distribution for comparison"
     ),
     
     # Show a plot of the generated distribution
